7e044e146e63a7aec5f8beef3484497ff3fca63c
Xinqi Zeng <2284182343@qq.com>
2024-09-28 08:36:59 +0000

    add 2 solve test and output to result.txt

--- test/test.cpp
+++ test/test.cpp
@@ -188,10 +188,76 @@ void sudokuGetSetTest(){
 
 void solveTest(){
     cout << "solve test" << endl;
+
+    cout << "string input" << endl;
+    cout << "original grid" << endl;
     std:string s = "017903600000080000900000507072010430000402070064370250701000065000030000005601720";
-    Sudoku sudoku(s);
-    sudoku.printGrid();
-    vector<vector<vector<int>>>result = sudoku.findAllPossibleValues();
+    Sudoku sudoku1(s);
+    sudoku1.printGrid();
+    vector<vector<vector<int>>>result = sudoku1.findAllPossibleValues();
+    cout << "result" << endl;
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << "row: " << i << " col: " << j << " possible values: ";
+            for (int k = 0; k < result[i][j].size(); k++)
+            {
+                cout << result[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+    }
+    cout << endl;
+
+    cout << "vector<int> input" << endl;
+    cout << "original grid" << endl;
+    vector<int> sudokuVec = {
+        5, 3, 0, 0, 7, 0, 0, 0, 0,
+        6, 0, 0, 1, 9, 5, 0, 0, 0,
+        0, 9, 8, 0, 0, 0, 0, 6, 0,
+        8, 0, 0, 0, 6, 0, 0, 0, 3,
+        4, 0, 0, 8, 0, 3, 0, 0, 1,
+        7, 0, 0, 0, 2, 0, 0, 0, 6,
+        0, 6, 0, 0, 0, 0, 2, 8, 0,
+        0, 0, 0, 4, 1, 9, 0, 0, 5,
+        0, 0, 0, 0, 8, 0, 0, 7, 9
+    };
+    Sudoku sudoku2(sudokuVec);
+    sudoku2.printGrid();
+    result = sudoku2.findAllPossibleValues();
+    cout << "result" << endl;
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << "row: " << i << " col: " << j << " possible values: ";
+            for (int k = 0; k < result[i][j].size(); k++)
+            {
+                cout << result[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+    }
+    cout << endl;
+
+    cout << "vector<vector<int>> input" << endl;
+    cout << "original grid" << endl;
+    vector<vector<int>> sudokuVec2D = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+    Sudoku sudoku3(sudokuVec2D);
+    sudoku3.printGrid();
+    result = sudoku3.findAllPossibleValues();
+    cout << "result" << endl;
     for (int i = 0; i < result.size(); i++)
     {
         for (int j = 0; j < result[i].size(); j++)
